84c3d345bc17b98a3469b988da5a66b2217d4f47
AIRIA <chaibingcheng0305@163.com>
2013-10-13 13:27:35 +0800

    背景添加了滚动效果

--- Classes/GameMain.cpp
+++ Classes/GameMain.cpp
@@ -14,8 +14,36 @@ bool GameMain::init()
         bRet = false;
     }
     SimpleAudioEngine::sharedEngine()->playBackgroundMusic("sound/game_music.mp3",true);
-    CCSprite *bg = CCSprite::createWithSpriteFrameName("background.png");
-    bg->setPosition(VisibleRect::center());
-    addChild(bg);
+    __initBackground();
+    
     return bRet;
+}
+
+void GameMain::__initBackground()
+{
+    m_pBgNode = CCSpriteBatchNode::createWithTexture(m_pFrameCache->spriteFrameByName("background.png")->getTexture());
+    m_pBgNode->setPosition(ccp(m_iWinSize.width/2,-120));
+    CCSprite *bg1 = CCSprite::createWithSpriteFrameName("background.png");
+    CCSprite *bg2 = CCSprite::createWithSpriteFrameName("background.png");
+    bg1->setAnchorPoint(ccp(0.5,0));
+    bg2->setAnchorPoint(ccp(0.5,0));
+    bg1->setPosition(ccp(0,0));
+    bg2->setPosition(ccp(0,m_nBgHeight-1));
+    m_pBgNode->addChild(bg1);
+    m_pBgNode->addChild(bg2);
+    addChild(m_pBgNode);
+    __scrollBackground();
+}
+
+void GameMain::__scrollBackground()
+{
+    if(m_pBgNode->getPositionY()!=0)
+    {
+        CCLog("循环一次完成");
+        m_pBgNode->setPositionY(0);
+    }
+    CCActionInterval *moveDown = CCMoveBy::create(109, ccp(0, -850));
+    CCCallFunc *resetHandler = CCCallFunc::create(this, callfunc_selector(GameMain::__scrollBackground));
+    CCSequence *moveSeq = CCSequence::create(moveDown,resetHandler,NULL);
+    m_pBgNode->runAction(moveSeq);
 }
--- Classes/GameMain.h
+++ Classes/GameMain.h
@@ -6,7 +6,13 @@
 
 class GameMain:public BaseScene
 {
+private:
+    void __initBackground();
+    void __scrollBackground();
+    CCSpriteBatchNode *m_pBgNode;
+    float m_nBgHeight;
 public:
+    GameMain():m_nBgHeight(851.5f){};
 	virtual void loadAssets();
 	virtual bool init();
 	CREATE_FUNC(GameMain);
